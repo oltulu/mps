0759ca6799166949ce18c81f0c738a8221af878f
Aylinux <cihanalk@gmail.com>
2021-03-08 23:37:23 +0300

    Update conf.lua

--- conf/conf.lua
+++ conf/conf.lua
@@ -3,19 +3,18 @@
 local mpsconf={
 	repo_dizin="/sources",
 	sunucu={
-		[1]="http://aylinux.kripto.com.tr/dosyalar/Aylinux-2021/depo",
+		[1]="http://paketler.aylinux.org",
+		--[2]="http://aylinux.kripto.com.tr/dosyalar/Aylinux-2021/depo",
 		--[1]="http://localhost:9999",
 	},
 	talimatdepo={
 		-- git repo adres, ilgili düzeye aktarılacak içerik
 		-- tname düzeylere göre
-		-- [1]={["https://mls.akdeniz.edu.tr/git/milislinux/milis19"]="talimatname/1"},
-		-- [2]={["https://mls.akdeniz.edu.tr/git/milislinux/milis19"]="talimatname/2"},
 		 [1]={["https://github.com/oltulu/Aylinux-2021"]="talimatlar"},
-      --  [4]={["https://mls.akdeniz.edu.tr/git/milis-topluluk/mkd"]="talimatname/test"},
+                --  [2]={["https://mls.akdeniz.edu.tr/git/milis-topluluk/mkd"]="talimatname/test"},
 		 -- [3]={
-		 --	["https://notabug.org/abc/milis"]="2/xorg",
-		 -- ["https://notabug.org/def/milis"]="2/xfce4",
+		 --	["https://notabug.org/abc/aylinux"]="2/xorg",
+		 -- ["https://notabug.org/def/aylinux"]="2/xfce4",
 		 --},
 		
 	},
