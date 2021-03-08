1aa34b9ea74d78fb1863e7d034d3c0196c2bc5ce
Aylinux <cihanalk@gmail.com>
2021-03-09 00:17:08 +0300

    Update conf.lua

--- conf/conf.lua
+++ conf/conf.lua
@@ -3,7 +3,7 @@
 local mpsconf={
 	repo_dizin="/sources",
 	sunucu={
-		[2]="http://paketler.aylinux.org",
+		--[2]="http://paketler.aylinux.org",
 		[1]="http://aylinux.kripto.com.tr/dosyalar/Aylinux-2021/depo",
 		--[1]="http://localhost:9999",
 	},
